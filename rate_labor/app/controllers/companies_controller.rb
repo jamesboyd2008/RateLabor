ef9bf3f06246ee572539d7e49d115391e85a8dad
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-05-01 14:44:36 -0700

    Update styling

--- rate_labor/app/controllers/companies_controller.rb
+++ rate_labor/app/controllers/companies_controller.rb
@@ -13,4 +13,22 @@ class CompaniesController < ApplicationController
     @reviews = Review.where(reviewable_type: "Company", reviewable_id: params[:id])
     @recruiters = Recruiter.where(company_id: params[:id])
   end
+
+  def edit
+    companies = Company.all.to_a.sort_by { |r| r.name }
+    @good_companies = []
+    @bad_companies = []
+    companies.each do |company|
+      reviews = Review.where(reviewable_type: "Company", reviewable_id: company.id)
+      positivities_of_reviews = []
+      reviews.each do |review|
+        positivities_of_reviews << review.positivity
+      end
+      if positivities_of_reviews.any? { |r| r == false }
+        @bad_companies << company
+      else
+        @good_companies << company
+      end
+    end
+  end
 end
