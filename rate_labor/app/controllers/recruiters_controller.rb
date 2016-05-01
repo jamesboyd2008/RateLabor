c2e921b66d30f31eb3d387e02384080ae5c508ef
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-05-01 12:11:54 -0700

    Columnize recruiters/index.erb

--- rate_labor/app/controllers/recruiters_controller.rb
+++ rate_labor/app/controllers/recruiters_controller.rb
@@ -1,5 +1,20 @@
 class RecruitersController < ApplicationController
   def index
+    recruiters = Recruiter.all.to_a.sort_by { |r| r.name }
+    @good_recruiters = []
+    @bad_recruiters = []
+    recruiters.each do |recruiter|
+      reviews = Review.where(reviewable_type: "Recruiter", reviewable_id: recruiter.id)
+      positivities_of_reviews = []
+      reviews.each do |review|
+        positivities_of_reviews << review.positivity
+      end
+      if positivities_of_reviews.any? { |r| r == false }
+        @bad_recruiters << recruiter
+      else
+        @good_recruiters << recruiter
+      end
+    end
   end
 
   def show
