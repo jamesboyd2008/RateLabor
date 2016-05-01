be0407a25e7b8fa7e63b6071fce42dc1b496ddc6
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-05-01 10:05:50 -0700

    Add styling to companies index

--- rate_labor/app/controllers/companies_controller.rb
+++ rate_labor/app/controllers/companies_controller.rb
@@ -1,5 +1,10 @@
 class CompaniesController < ApplicationController
   def index
-    @companies = Company.all;
+    companies = Company.all.to_a;
+    third = companies.length / 3
+    # do this in the model
+    @companies_1 = companies.slice!(0..third)
+    @companies_2 = companies.slice!(0..third)
+    @companies_3 = companies
   end
 end
